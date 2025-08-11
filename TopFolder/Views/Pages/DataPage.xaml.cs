fe6c85f2a0e4e9c548d297e820a3e7f5b0c21bb6
100jnghn <gnswhd0415@gmail.com>
2025-08-11 18:38:10 +0900

    FEAT : DataPage Front제작
     - Grid 0, 1, 2
     - DataGrid 생성

--- TopFolder/Views/Pages/DataPage.xaml.cs
+++ TopFolder/Views/Pages/DataPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using InstagramManager.ViewModels.Pages;
+using System.Diagnostics;
+using System.Windows.Navigation;
 using Wpf.Ui.Abstractions.Controls;
 
 namespace InstagramManager.Views.Pages
@@ -13,6 +15,11 @@ namespace InstagramManager.Views.Pages
             DataContext = this;
 
             InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e) {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+            e.Handled = true;
         }
     }
 }
