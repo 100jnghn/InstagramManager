5a0f5280b990cd5460c5ed8e329ff87b58440a92
100jnghn <gnswhd0415@gmail.com>
2025-08-11 16:01:24 +0900

    FEAT : DataPage 초기화
     - 기존의 양식을 지우고 백지 상태로 만들기

--- TopFolder/ViewModels/Pages/DataViewModel.cs
+++ TopFolder/ViewModels/Pages/DataViewModel.cs
@@ -8,9 +8,7 @@ namespace InstagramManager.ViewModels.Pages
     {
         private bool _isInitialized = false;
 
-        [ObservableProperty]
-        private IEnumerable<DataColor> _colors;
-
+        
         public Task OnNavigatedToAsync()
         {
             if (!_isInitialized)
@@ -20,29 +18,10 @@ namespace InstagramManager.ViewModels.Pages
         }
 
         public Task OnNavigatedFromAsync() => Task.CompletedTask;
-
+            
         private void InitializeViewModel()
         {
-            var random = new Random();
-            var colorCollection = new List<DataColor>();
-
-            for (int i = 0; i < 8192; i++)
-                colorCollection.Add(
-                    new DataColor
-                    {
-                        Color = new SolidColorBrush(
-                            Color.FromArgb(
-                                (byte)200,
-                                (byte)random.Next(0, 250),
-                                (byte)random.Next(0, 250),
-                                (byte)random.Next(0, 250)
-                            )
-                        )
-                    }
-                );
-
-            Colors = colorCollection;
-
+            
             _isInitialized = true;
         }
     }
